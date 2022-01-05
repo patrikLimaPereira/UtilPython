4521726de2fb2853769c348d3a0aa9e6cb9ebad9
patricrenner <patricrenner@outlook.com>
2022-01-05 11:04:18 -0300

    código refatorado

--- WorkFiles.py
+++ WorkFiles.py
@@ -1,45 +1,35 @@
-
 from datetime import datetime
 
 
-def gravaArquivo(nomeDoArquivo ,txt):
-		try:
-			arquivo = open(nomeDoArquivo,'w+')
-			arquivo.write(txt)
-		except Exception:
-			print(Exception.with_traceback)
-		
-		finally:
-			arquivo.close()
-			
-			
+def gravaArquivo(nomeDoArquivo, txt):
+    try:
+        arquivo = open(nomeDoArquivo, 'w+')
+        arquivo.write(txt)
+    except Exception:
+        print(Exception.with_traceback)
 
-def atualizaArquivo(nomeDoArquivo ,txt):
-		try:
-			arquivo = open(nomeDoArquivo,'a+')
-			arquivo.write(txt)
-		except Exception:
-			print(Exception.with_traceback)
-		    
-		finally:
-			arquivo.close()
-			
-			
-def log (txt):	
-  	  data =  datetime.now().strftime('%d/%m/%Y %H:%M')  	  
-  	  nomeDoArquivo = "log.txt"
-  	  txtLog = data +'\n'
-  	  txtLog = +txtLog+txt+'\n\n\n'
-  	  atualizaArquivo(nomeDoArquivo,txtLog)		
-  	  
-  	  
-	
-	
+    finally:
+        arquivo.close()
+
+
+def atualizaArquivo(nomeDoArquivo, txt):
+    try:
+        arquivo = open(nomeDoArquivo, 'a+')
+        arquivo.write(txt)
+    except Exception:
+        print(Exception.with_traceback)
+
+    finally:
+        arquivo.close()
+
+
+def log(txt):
+    data = datetime.now().strftime('%d/%m/%Y %H:%M')
+    nomeDoArquivo = "log.txt"
+    txtLog = data + '\n'
+    txtLog = +txtLog + txt + '\n\n\n'
+    atualizaArquivo(nomeDoArquivo, txtLog)
+
+
 def lerArquivo():
-	return None
-	
-	
-
-
-
-
+    return None
